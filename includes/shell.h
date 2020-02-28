85a336e24d22a8f58d428cfe1b04c717b0626d06
Zouheir Layine <zlayine@e2r7p2.1337.ma>
2020-02-28 11:40:22 +0100

    header

--- includes/shell.h
+++ includes/shell.h
@@ -6,12 +6,35 @@
 /*   By: zlayine <zlayine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/28 10:50:30 by zlayine           #+#    #+#             */
-/*   Updated: 2020/02/28 11:31:00 by zlayine          ###   ########.fr       */
+/*   Updated: 2020/02/28 11:40:08 by zlayine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SHELL_H
+# define SHELL_H
+
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
+# include <dirent.h>
+# include <stdio.h>
+# include "../libft/libft.h"
+# define BUFFER_SIZE 1
+
 typedef struct  s_shell
 {
-    int     a;
-    char    *str;
+    char    **env;
+    t_cmds  cmds;
 }               t_shell;
+
+typedef struct  s_cmds
+{
+    char    *cmd;
+    char    **args;
+    void    *next;
+}               t_cmds;
+
+#endif
