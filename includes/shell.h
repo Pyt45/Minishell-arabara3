45d252804beeacb30a474f792de253cd6552ee5f
zlayine <zlayine@student.1337.ma>
2020-02-28 18:46:36 +0100

    pipe_parse

--- includes/shell.h
+++ includes/shell.h
@@ -6,7 +6,7 @@
 /*   By: zlayine <zlayine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/28 10:50:30 by zlayine           #+#    #+#             */
-/*   Updated: 2020/02/28 16:55:32 by zlayine          ###   ########.fr       */
+/*   Updated: 2020/02/28 18:45:00 by zlayine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,22 @@
 # include <dirent.h>
 # include <stdio.h>
 # include "../libft/libft.h"
-# define BUFFER_SIZE 1
 
-typedef struct  s_cmds
+typedef struct		s_cmds
 {
-    char    *cmd;
-    char    **args;
-    t_cmds  *next;
-}               t_cmds;
+    char			*cmd;
+    char			**args;
+    struct s_cmds 	*next;
+}					t_cmds;
 
-typedef struct  s_shell
+typedef struct		s_shell
 {
-    char    **env;
-    char    **result;
-    t_cmds  cmds;
-}               t_shell;
+    char			**env;
+    struct s_cmds   cmds;
+}					t_shell;
+
+char    *get_cmd(char *str);
+char    **get_args(char *str);
+t_cmds  *get_commands(char *str);
 
 #endif
--- srcs/pipe_parse.c
+++ srcs/pipe_parse.c
@@ -6,7 +6,63 @@
 /*   By: zlayine <zlayine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/28 15:12:59 by zlayine           #+#    #+#             */
-/*   Updated: 2020/02/28 15:13:03 by zlayine          ###   ########.fr       */
+/*   Updated: 2020/02/28 18:45:33 by zlayine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/shell.h"
+
+char    *get_cmd(char *str)
+{
+    int     i;
+    char    *cmd;
+
+    i = 0;
+    while (!ft_isalpha(*str))
+        str++;
+    while (ft_isalpha(str[i]))
+        i++;
+    cmd = malloc(sizeof(char) * (++i));
+    ft_strlcpy(cmd, str, i);
+    return (cmd);
+}
+
+char    **get_args(char *str)
+{
+    int     i;
+
+    i = 0;
+    while (!ft_isalpha(*str))
+        str++;
+    while (ft_isalpha(*str))
+        str++;
+    return (ft_split(str, ' '));
+}
+
+t_cmds  *get_commands(char *str)
+{
+    t_shell shell;
+    t_cmds  *cmds;
+    t_cmds  *head;
+    char **tmp = ft_split(str, '|');
+
+    if (*tmp)
+    {
+        cmds = malloc(sizeof(t_cmds));
+        head = cmds;
+    }
+    while (*tmp)
+    {
+        cmds->cmd = get_cmd(*tmp);
+        printf("COMMAND: %s|\n", cmds->cmd);
+        cmds->args = get_args(*tmp);
+        printf("ARG1: %s|\n", cmds->args[0]);
+        tmp++;
+        if (*tmp)
+        {
+            cmds->next = malloc(sizeof(t_cmds));
+            cmds = cmds->next;
+        }
+    }
+    return (head);
+}
