86883cc13132105f3170421c1cf494647de3dbf0
aaqlzim <ayoub1234aqlzim@gmail.com>
2020-02-28 10:55:34 +0100

    main.c

---
+++ srcs/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int     main(int argc, char **argv, char **env)
+{
+    if (argc)
+    {
+        char    **envs = env;
+        while (*envs)
+        {
+            printf("%s\n", *envs);
+            envs++;
+        }
+    }
+    return (0);
+}
