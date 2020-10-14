ed00b9eb4f87c8252496d7590e7d9cb9152c7f8a
aaqlzim <ayoub1234aqlzim@gmail.com>
2020-10-14 09:33:34 +0100

    fuckit

--- srcs/exec_commands.c
+++ srcs/exec_commands.c
@@ -37,6 +37,7 @@ int     exec_commands(t_shell *shell, t_cmds *cmds)
         return (unset_builtin(shell, cmds)* 0 + 1);
     else if (!ft_strcmp(cmds->cmd, "echo"))
        return (echo_builtin(cmds, shell->env));
+
     return (0);
 }
 
@@ -45,7 +46,7 @@ int		*pipe_fds(int num_pipe, int *fds)
 	int i;
 
 	i = 0;
-	fds = malloc(sizeof(int) * num_pipe);
+	fds = malloc(sizeof(int) * (2 * num_pipe));
 	while (i < num_pipe)
 	{
 		if (pipe(fds + i * 2) < 0)
@@ -60,6 +61,14 @@ int		*pipe_fds(int num_pipe, int *fds)
 
 int		*create_fds(t_cmds *cmds, int j, int *fds)
 {
+	if (j != 0)
+	{
+		if (dup2(fds[j - 2], 0) < 0)
+		{
+			perror("2.|dup2|");
+			exit(EXIT_FAILURE);
+		}
+	}
 	if (cmds->next)
 	{
 		if (dup2(fds[j + 1], 1) < 0)
@@ -68,112 +77,204 @@ int		*create_fds(t_cmds *cmds, int j, int *fds)
 			exit(EXIT_FAILURE);
 		}
 	}
+	return (fds);
+}
+
+int		*create_ior(t_cmds *cmd, int j, int *fds)
+{
 	if (j != 0)
 	{
 		if (dup2(fds[j - 2], 0) < 0)
 		{
-			perror("2.|dup2|");
+			perror("3.|dup2|");
+			exit(EXIT_FAILURE);
+		}
+	}
+	if (cmd->next)
+	{
+		if (dup2(fds[j + 1], 1) < 0)
+		{
+			perror("4.|dup2|");
+			exit(EXIT_FAILURE);
 		}
 	}
 	return (fds);
 }
 
-t_cmds     *excute_command_by_order(t_shell *shell, t_cmds *cmds, int num_pipe)
+int		*pipe_ior(int num_sp, int *ior)
+{
+	int i;
+
+	i = 0;
+	ior = malloc(sizeof(int) * (2 * num_sp));
+	while (i < num_sp)
+	{
+		if (pipe(ior + i * 2) < 0)
+		{
+			perror("couldn't pipe");
+			exit(EXIT_FAILURE);
+		}
+		i++;
+	}
+	return (ior);
+}
+
+
+int			open_output(t_cmds *cmd, int append, int ofd)
+{
+	int		fd;
+	int		flag;
+
+	flag = O_WRONLY | O_CREAT;
+	if (append)
+		flag = flag | O_APPEND;
+	else
+		flag = flag | O_TRUNC;
+	if ((fd = open(cmd->next->args[0], flag, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH)) >= 0)
+		ofd = fd;
+	dup2(ofd, 1);
+	return (ofd);
+}
+
+void	read_from_stdin(int fd)
+{
+	char	buff[1024];
+	int		r;
+
+	while ((r = read(0, buff, 1024)))
+	{
+		if (write(fd, buff, r) < r)
+			return ;
+	}
+}
+
+int		open_input(char *args, int append, int ifd)
+{
+	int		fd;
+	int		pipefd[2];
+	
+	if (append)
+	{
+		pipe(pipefd);
+		read_from_stdin(pipefd[1]);
+		close(pipefd[1]);
+		ifd = pipefd[0];
+	}
+	else if ((fd = open(args, O_RDONLY)) >= 0)
+	{
+		//puts("here\n");
+		ifd = fd;
+	}
+	dup2(ifd, 1);
+	return (ifd);
+}
+
+void		do_redirect(t_cmds *cmd, int fd[2])
+{
+	if (cmd->append == -2) // <<
+		fd[0] = open_input(cmd->args[1], 1, fd[0]);
+	else if (cmd->append == 2) // >>
+	{
+		fd[1] = open_output(cmd, 1, fd[1]);
+		//printf("%s %s\n", cmd->args[1], cmd->next->args[0]);
+	}
+	if (cmd->append == 1) // >
+		fd[1] = open_output(cmd, 0, fd[1]);
+	else if (cmd->append == -1) // <
+		fd[1] = open_input(cmd->next->args[0], 0, fd[1]);
+}
+
+void		exec_io_redi(t_cmds *cmd, int ifd, int ofd, t_shell *shell)
+{
+	int		new_fd[2];
+
+	new_fd[0] = ifd;
+	new_fd[1] = ofd;
+
+	do_redirect(cmd, new_fd);
+	if (new_fd[0] != ifd)
+		close(new_fd[0]);
+	if (new_fd[1] != ofd)
+		close(new_fd[1]);
+}
+
+t_cmds     *excute_command_by_order(t_shell *shell, t_cmds *cmds, int num_pipe, int num_sp)
 {
 	pid_t	pid;
 	int 	status = 0;
+	char	*line;
+	char	*content;
+	int		r = 1;
     int		i = 0;
+	int		*ior;
 	int		*fds;
 	int		j = 0;
 	
+	//num_pipe = 1;
+	(num_pipe) ? fds = pipe_fds(num_pipe, fds) : 0;
+	(num_sp) ? fds = pipe_ior(num_sp, fds) : 0;
 	if ((cmds->next && !cmds->end) || !exec_commands(shell, cmds))
 	{
-<<<<<<< HEAD
-		for (i = 0; i < 2 * num_pipe; i++)
-		{
-			if (pipe(fds + i * 2) < 0)
-			{
-				perror("couldn't pipe");
-            	exit(EXIT_FAILURE);
-			}
-		}
-=======
-		fds = pipe_fds(num_pipe, fds);
->>>>>>> fe983a05605c1abc1dc2442c63e3b09fe58e34b5
+		//fds = pipe_fds(num_pipe, fds);
 		j = 0;
 		while (cmds)
 		{
+			save_restor_fd(1,0);
 			pid = fork();
 			if (pid == 0)
 			{
-<<<<<<< HEAD
-				//child outputs to next command, if it's not the last command
-				if (j != 0)
+
+				(num_pipe) ? fds = create_fds(cmds, j, fds) : 0;
+				for (i = 0; i < 2 * num_pipe; i++)
+					close(fds[i]);
+				if (cmds->append != 0 || (cmds->prev && cmds->prev->append))
 				{
-					if (dup2(fds[(j - 1) * 2], 0) < 0)
+					ior[0] = 0;
+					ior[1] = fds[1];
+					exec_io_redi(cmds, ior[0], ior[1], shell);
+					if ((!exec_commands(shell, cmds) && (execve(get_bin_path(cmds->cmd, shell->env), cmds->args, shell->env) < 0)))
 					{
-						perror("2.|dup2|");
+						perror("cmd1");
+						//exit(EXIT_FAILURE);
 					}
+					close(ior[1]);
+					ior[1] = 1;
 				}
-				// child gets input from the previous command, if it's not the first command
-				if (cmds->next)
+				else
 				{
-					if (dup2(fds[j * 2 + 1], 1) < 0)
+					if ((!exec_commands(shell, cmds) && (execve(get_bin_path(cmds->cmd, shell->env), cmds->args, shell->env) < 0)))
 					{
-						perror("1.|dup2|");
+						perror("cmd");
 						exit(EXIT_FAILURE);
 					}
 				}
-				for (i = 0; i < 2 * num_pipe; i++)
-=======
-				fds = create_fds(cmds, j, fds);
-				for (i = 0; i < 2 *  num_pipe; i++)
->>>>>>> fe983a05605c1abc1dc2442c63e3b09fe58e34b5
-					close(fds[i]);
-				if (!exec_commands(shell, cmds) && execve(get_bin_path(cmds->cmd, shell->env), cmds->args, shell->env) < 0)
-				{
-					perror("cmd");
-					exit(EXIT_FAILURE);
-				}
-				dprintf(2, "CMD=%s\n", cmds->cmd);
-				printf("CMD=%s\n", cmds->cmd);
+				exit(0);
 			}
-<<<<<<< HEAD
 			/* else if (pid < 0)
 			{
 				perror("Error");
 				exit(EXIT_FAILURE);
 			} */
-			else
-			{
-				if (cmds != NULL)
-					cmds = cmds->next;
-				j++;
-				for (i = 0; i < 2 * num_pipe; i++)
-					close(fds[i]);
-				while (waitpid(pid, &status, 0) < 0);
-			}
-		}
-=======
-			// else if (pid < 0)
-			// {
-			// 	perror("Error");
-			// 	exit(EXIT_FAILURE);
-			// }
 			if (cmds->end)
 				break;
 			else
 				cmds = cmds->next;
 			j += 2;
+			save_restor_fd(0,1);
 		}
 		for (i = 0; i < 2 * num_pipe; i++)
 			close(fds[i]);
-	/* 	i = -1;
-		while (++i < num_pipe)
-			wait(&status); */
-		while (waitpid(pid, &status, 0) < 0);
+		i = -1;
+		if (!num_pipe)
+			waitpid(pid, &status, 0);
+		else
+		{
+			while (++i < 2 * num_pipe)
+			{
+				wait(&status);
+			}
+		}
 		free(fds);
->>>>>>> fe983a05605c1abc1dc2442c63e3b09fe58e34b5
 	}
     return (cmds);
 }
@@ -191,6 +292,19 @@ int		get_num_pipes(t_cmds *cmds)
 	return (i);
 }
 
+/* int		get_num_sp(t_cmds *cmds)
+{
+	int	i;
+
+	i = 0;
+	while (cmds->s)
+	{
+		i++;
+		cmds = cmds->next;
+	}
+	return (i);
+} */
+
 int		run_commands(t_shell *shell)
 {
 	t_cmds	*cmds;
@@ -199,31 +313,10 @@ int		run_commands(t_shell *shell)
 	cmds = shell->cmds;
 	while (cmds)
 	{
-		save_restor_fd(1,0);
-		cmds = excute_command_by_order(shell, cmds, get_num_pipes(cmds));
-		save_restor_fd(0,1);
+		//save_restor_fd(1,0);
+		cmds = excute_command_by_order(shell, cmds, get_num_pipes(cmds), 1);
+		//save_restor_fd(0,1);
 		cmds = cmds->next;
 	}
 	return (1);
 }
-
-<<<<<<< HEAD
-/* int main(int args, char **argv, char **env)
-{
-	t_shell *shell;
-=======
-// int main(int args, char **argv, char **env)
-// {
-// 	t_shell *shell;
->>>>>>> fe983a05605c1abc1dc2442c63e3b09fe58e34b5
-
-// 	shell = malloc(sizeof(t_shell));
-// 	shell->env = ft_arrdup(env);
-// 	shell->line = ft_strdup("env | grep PATH ; env | grep PWD ; ls /bin | grep mkdir");
-// 	execute_command(shell);
-
-<<<<<<< HEAD
-} */
-=======
-// }
->>>>>>> fe983a05605c1abc1dc2442c63e3b09fe58e34b5
