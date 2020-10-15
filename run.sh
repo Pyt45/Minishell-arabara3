43a8702f5e9c0c9ad1a1c5cc48e7c508af9d37c6
zlayine <zlayine@student.1337.ma>
2020-10-15 10:12:54 +0100

    fixed run

--- run.sh
+++ run.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-<<<<<<< HEAD
-<<<<<<< HEAD
 make && ./minishell
-=======
-gcc srcs/handle_path.c srcs/handle_prompt.c srcs/utils.c srcs/commands_parse.c srcs/echo_builtin.c srcs/env_builtin.c srcs/exit_builtin.c srcs/export_builtin.c srcs/unset_builtin.c srcs/main.c srcs/cd_builtin.c libft.a srcs/exec_commands.c srcs/pwd_builtin.c && ./a.out 
->>>>>>> 5e28422ba627577f9000067e98a03be129e89a50
-=======
-gcc srcs/handle_path.c srcs/handle_prompt.c srcs/utils.c srcs/commands_parse.c srcs/echo_builtin.c srcs/env_builtin.c srcs/exit_builtin.c srcs/export_builtin.c srcs/unset_builtin.c srcs/main.c srcs/cd_builtin.c libft.a srcs/exec_commands.c srcs/pwd_builtin.c && ./a.out 
->>>>>>> 08cef678732dcaef9eea8156554b6eb400e1a5f0
