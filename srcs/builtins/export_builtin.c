2582983372ec062eae58a2cbbe9efb4ccec476b6
aaqlzim <ayoub1234aqlzim@gmail.com>
2020-10-21 11:50:08 +0100

    export

--- srcs/builtins/export_builtin.c
+++ srcs/builtins/export_builtin.c
@@ -2,15 +2,79 @@
 #include "../../includes/shell.h"
 
 
+static char	**ft_sort_export(char **env)
+{
+	int 	i;
+	int		j;
+	char	*tmp;
+
+	i = 0;
+	if (env)
+	{
+		while (i < ft_arr_len(env))
+		{
+			j = i + 1;
+			while (j < ft_arr_len(env))
+			{
+				if (env[i][0] > env[j][0])
+				{
+					tmp = env[i];
+					env[i] = env[j];
+					env[j] = tmp;
+				}
+				j++;
+			}
+			i++;
+		}
+	}
+	return (env);
+}
+
+static void ft_export_join(char **env)
+{
+	char	**new_env;
+	int		i = 0;
+	//int		j = 0;
+
+	if (env)
+	{
+		while (env[i] != NULL)
+		{
+			if (ft_strchr(env[i], '='))
+			{
+				new_env = ft_split(env[i], '=');
+				printf("|%s|\n", new_env[i] + 1);
+			}
+			i++;
+		}
+		//ft_print_env_arr(new_env);
+	}
+}
+
 int     export_builtin(t_shell *shell, t_cmds *cmds)
 {
 	int		i;
+	char	**new_env;
 
 	i = 1;
-	while (cmds->args[i])
+	/* while (cmds->args[i])
 	{
 		shell->env = ft_export_cmd(shell, cmds->args[i]);
 		i++;
+	} */
+	if (cmds->args[1] != NULL)
+	{
+		while (cmds->args[i])
+		{
+			shell->env = ft_export_cmd(shell, cmds->args[i]);
+			i++;
+		}
+	}
+	else
+	{
+		//new_env = ft_sort_export(shell->env);
+		//ft_print_env_arr(new_env);
+		 ft_export_join(shell->env);
 	}
 	return (0);
 }
